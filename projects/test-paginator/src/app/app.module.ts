f44b6edd653cd6fb06b3fdbf25a255f76cc389b2
Joël Maizi <joel.maizi@lirmm.fr>
2023-06-23 08:19:56 +0200

    ng g st AppState...

--- projects/test-paginator/src/app/app.module.ts
+++ projects/test-paginator/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DepartementModule } from 'projects/test-paginator/departement/departement.module';
+import { CommuneModule } from 'projects/test-paginator/commune/commune.module';
+import { StoreModule } from '@ngrx/store';
+import * as fromAppState from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     CommonModule,
-    BrowserModule
+    BrowserModule,
+    DepartementModule,
+    CommuneModule,
+    StoreModule.forRoot(fromAppState.reducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: false,
+      autoPause: true,
+      features: {
+        pause: false,
+        lock: true,
+        persist: true
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
