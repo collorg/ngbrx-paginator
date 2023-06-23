fea3e272085007f34e6687100254f6b4e768c5e8
Joël Maizi <joel.maizi@lirmm.fr>
2023-06-23 09:54:16 +0200

    Add collection$ to departement component.

--- projects/test-paginator/src/app/departement/departements/departements.component.ts
+++ projects/test-paginator/src/app/departement/departements/departements.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { departements } from '../../data/departements';
 import { Store } from '@ngrx/store';
 import { DepartementActions } from '../departement.actions';
+import { Observable } from 'rxjs';
+import { Departement } from '../departement.model';
+import * as fromStore from '../departement.reducer';
 
 @Component({
   selector: 'app-departements',
@@ -9,6 +12,7 @@ import { DepartementActions } from '../departement.actions';
   styleUrls: ['./departements.component.css']
 })
 export class DepartementsComponent {
+  collection$: Observable<Departement[]> = this.store.select(fromStore.departementsFeature.selectAll)
   constructor(
     private store: Store
   ) {
